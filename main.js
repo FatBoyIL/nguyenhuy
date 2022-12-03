477fb13862a7f48596a9650878fc1126cd77a3e8
FIL2703 <huynguyenhonggia2703@gmail.com>
2022-12-03 22:44:55 +0700

    update

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-var input1=document.getElementById("input1").value;
-var input2=document.getElementById("input2").value;
-var input3=document.getElementById("input3").value;
+
+
 function loginshop()
 {
     var input1=document.getElementById("input1").value;
@@ -40,6 +39,38 @@ function logincom()
     }
     else{
         alert("Account Success Login");
-        window.location.href="./index.html"
+        window.location.href="./community.html"
+    }
+}
+function loginsign()
+{
+    var input1=document.getElementById("input1").value;
+    var input2=document.getElementById("input2").value;
+    var input3=document.getElementById("input3").value;
+    if(input1 == " ")
+    {
+        alert("Apply Your Account")
+    }
+    else{
+        alert("Account Success Login");
+        window.location.href="./signup.html"
+    }
+}
+function signup()
+{
+    var sign1=document.getElementById("sign1").value;
+    var sign2=document.getElementById("sign2").value;
+    var sign3=document.getElementById("sign3").value;
+    var sign4=document.getElementById("sign4").value;
+    var sign5=document.getElementById("sign5").value;
+    var sign6=document.getElementById("sign6").value;
+    
+    if(sign1 == " ")
+    {
+        alert("Apply Your Account")
+    }
+    else{
+        alert("Sign up success!");
+        window.location.href="./signup.html"
     }
 }
