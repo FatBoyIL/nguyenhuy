4b75dd04785ea838af714db1bd3017fc9b127d06
FIL2703 <huynguyenhonggia2703@gmail.com>
2022-12-03 19:23:00 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+var input1=document.getElementById("input1").value;
+var input2=document.getElementById("input2").value;
+var input3=document.getElementById("input3").value;
+function loginshop()
+{
+    var input1=document.getElementById("input1").value;
+    var input2=document.getElementById("input2").value;
+    var input3=document.getElementById("input3").value;
+    if(input1 == " ")
+    {
+        alert("Apply Your Account")
+    }
+    else{
+        alert("Account Success Login");
+        window.location.href="./shop.html"
+    }
+}
+function loginhome()
+{
+    var input1=document.getElementById("input1").value;
+    var input2=document.getElementById("input2").value;
+    var input3=document.getElementById("input3").value;
+    if(input1 == " ")
+    {
+        alert("Apply Your Account")
+    }
+    else{
+        alert("Account Success Login");
+        window.location.href="./index.html"
+    }
+}
+function logincom()
+{
+    var input1=document.getElementById("input1").value;
+    var input2=document.getElementById("input2").value;
+    var input3=document.getElementById("input3").value;
+    if(input1 == " ")
+    {
+        alert("Apply Your Account")
+    }
+    else{
+        alert("Account Success Login");
+        window.location.href="./index.html"
+    }
+}
